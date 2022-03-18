012ea412c88de8876a93880d245defc44cc86c7a
wvxbs <gabriel.ferreira7854@gmail.com>
2022-03-17 21:27:29 -0300

    index.js added

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('exoress)
+const app = express()
+const port = 3001
+const ip = require("ip")
+
+app.use((req, res, next) => {
+    res.setHeader('Acess-Control-Allow-Origin', "*")
+    res.setHeader('Acess-Control-Allow-Headers', "*")
+    res.setHeader('Acess-Control-Allow-Credentials', "true")
+    next()
+})
+
+app.listen(port, () => {
+    console.log(`Server running at ${ip.adress()}:${port}`)
+})
