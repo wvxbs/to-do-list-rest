41e7ba4578aa5d462248c969894da695dd68ba1e
wvxbs <gabriel.ferreira7854@gmail.com>
2022-03-28 09:40:47 -0300

    mongodb connection added

--- Server.js
+++ Server.js
@@ -1,3 +1,17 @@
+const mongoose = require('mongoose')
+require('dotenv').config({  path : '.env'})
+
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+
+mongoose.Promise = global.Promise
+
+mongoose.connection.on("error", (err) => {
+    console.error(`Database connection error -> ${err.message}`)
+})
+
 const app = require('./App')
 
 const server = app.listen(3001, () => {
